da9ca0a9f5eda1c8122510fbccd3ed72dcd77f2f
Nelson <nelsonlho@gmail.com>
2015-06-30 00:50:35 -0700

    Added Google Map API

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 //  OpenShift sample Node application
 var express = require('express');
 var fs      = require('fs');
+var http    = require('http');
+var cheerio = require('cheerio');
+var xml2js  = require('xml2js');
+var request = require('request');
+
 
 
 /**
@@ -104,7 +109,121 @@ var SampleApp = function() {
             res.setHeader('Content-Type', 'text/html');
             res.send(self.cache_get('index.html') );
         };
+
+        self.routes['/expedia'] = function(req, res) {
+
+            if(!req.query.place){
+                res.send({"status":"fail", "msg":"please send place"});
+                return false;
+            }
+
+            var place = req.query.place;
+            defaultStart='08/03/2015';
+            defaultEnd='08/04/2015';
+            place= encodeURIComponent(place);
+            xml1 = '<HotelListRequest><city>'+place+'</city><arrivalDate>'+defaultStart+'</arrivalDate><departureDate>'+defaultEnd+'</departureDate><RoomGroup><Room><numberOfAdults>2</numberOfAdults></Room></RoomGroup><numberOfResults>25</numberOfResults></HotelListRequest>';
+
+
+            console.log('http://api.ean.com/ean-services/rs/hotel/v3/list?cid=55505&minorRev=99&apiKey=cbrzfta369qwyrm9t5b8y8kf&locale=en_US&currencyCode=USD&xml='+encodeURIComponent(xml1));
+
+            self.download('http://api.ean.com/ean-services/rs/hotel/v3/list?cid=55505&minorRev=99&apiKey=cbrzfta369qwyrm9t5b8y8kf&locale=en_US&currencyCode=USD&xml='+encodeURIComponent(xml1), function(res1){
+                
+                console.log(res1);
+                res.send(res1);
+
+                
+            });
+
+        };
+
+        self.routes['/airbnb'] = function(req, res) {
+            if (!req.query.city || !req.query.state){
+              res.send({"status":"fail", "msg":"please tell us which city you want to search for (i.e. ?city=San Francisco&state=CA"});
+                return false;
+            }
+            var state = "";
+            
+            if (req.query.state !== null){
+                state= req.query.state;
+            }
+            var address = req.query.city+state;
+
+            var googleMapOptions = {
+              method:'POST',
+              url: 'http://maps.googleapis.com/maps/api/geocode/json?address='+address,
+              headers: {
+              }
+            };
+
+            request(googleMapOptions, function(err, data){
+                var json = JSON.parse(data.body);
+               // console.log(data.body);
+               var lat = json.results[0].geometry.location.lat;
+               var lon = json.results[0].geometry.location.lng;
+               var options = {
+               method:'POST',
+              url: 'https://zilyo.p.mashape.com/search?latitude='+lat+'&longitude='+lon,
+              headers: {
+                'X-Mashape-Key': 'tcuqOjloPemshpTEzhbU4Kr7R2EHp1a7HOTjsnnrSYS5cOETim','Accept': 'application/json'
+              }
+            };
+
+            request(options, function(err, data){
+
+
+                res.send(data.body);
+             });
+              //  console.log(lat + "asdf"+lon);
+
+               // res.send(json.results[0].geometry.location.lng);
+            });
+            /*
+             if(!req.query.lat || !req.query.lon){
+                res.send({"status":"fail", "msg":"please send plat and lon. eg ?lat=40.75458&lon=73.97749"});
+                return false;
+            }
+
+            var lat = req.query.lat
+            var lon= req.query.lon
+            */
+            
+           /* var options = {
+                method:'POST',
+              url: 'https://zilyo.p.mashape.com/search?latitude='+lat+'&longitude='+lon,
+              headers: {
+                'X-Mashape-Key': 'tcuqOjloPemshpTEzhbU4Kr7R2EHp1a7HOTjsnnrSYS5cOETim','Accept': 'application/json'
+              }
+            };
+
+            request(options, function(err, data){
+
+
+                res.send(data.body);
+            });
+*/
+
+
+
+           
+        };
+
+
     };
+
+
+    self.download=function(url, callback) {
+              http.get(url, function(res) {
+                var data = "";
+                res.on('data', function (chunk) {
+                  data += chunk;
+                });
+                res.on("end", function() {
+                  callback(data);
+                });
+              }).on("error", function() {
+                callback(null);
+              });
+    }
 
 
     /**
